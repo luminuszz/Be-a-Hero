404499b7ca41f45153ec916b19c554a5ba6622d1
luminuszz <luminuszz43@gmail.com>
2020-03-30 16:10:33 -0300

    .

--- src/app/config/database.ts
+++ src/app/config/database.ts
@@ -1,8 +1,13 @@
 export const databaseConfig = {
-  client: 'sqlite3',
+  client: 'pg',
   connection: {
-    filename: 'db.sqlite'
+    host: 'ec2-18-235-97-230.compute-1.amazonaws.com',
+    user: 'ikjkkpwypyxmlw',
+    password: '548f8c5c89b6228e387439d891ecaea9a74c27738ee0cecfc8900d5445cf509d',
+    database: 'd5vtrq1jomf8lc',
+    port: 5432
   },
+
   migrations: {
     directory: 'src/app/database/migrations'
   },
