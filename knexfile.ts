51085b48e838c3582efd2ae465ed9332e7d072bb
luminuszz <luminuszz43@gmail.com>
2020-03-31 14:27:29 -0300

    .

--- knexfile.ts
+++ knexfile.ts
@@ -4,5 +4,4 @@ import pg from 'pg'
 import { databaseConfig } from './src/app/config/database'
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 pg.defaults.ssl = true
-
 module.exports = databaseConfig
