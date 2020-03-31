cc7bf686a2efca2172c32fe6c5d40c85703d8d67
luminuszz <luminuszz43@gmail.com>
2020-03-31 15:10:51 -0300

    .

--- knexfile.ts
+++ knexfile.ts
@@ -1,9 +1,7 @@
 /* eslint-disable no-unused-expressions */
 
-import pg from 'pg'
+import postgres from 'pg'
 
 import { databaseConfig } from './src/app/config/database'
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
-pg.defaults.ssl = true
 module.exports = databaseConfig
--- src/app/database/connection.ts
+++ src/app/database/connection.ts
@@ -1,5 +1,8 @@
 /* eslint-disable no-undef */
 import knex from 'knex'
+import postgres from 'pg'
 
 import { databaseConfig } from '../config/database'
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+postgres.defaults.ssl = true
 export const connect = knex(databaseConfig)
